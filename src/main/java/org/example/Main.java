e7f017d8163c18829619038a71edc648ed030513
Kostiantyn Poberezhniuk <konspbk@gmail.com>
2026-02-08 01:54:25 +0000

    maven fatJar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 package org.example;
 
 
-public class Main {
-    static void main(String[] args) {
+import com.google.gson.Gson;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> person = new HashMap<>();
+
+        person.put("name:", "John");
+        person.put("LastName", "Smith");
+
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+
+        System.out.println(json);
 
     }
 }
